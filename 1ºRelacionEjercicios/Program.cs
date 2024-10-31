30961a68f01988ad5032a29b8e1b324584568a27
Samuel Avella <samu.av.jm18@gmail.com>
2024-10-31 10:01:43 +0100

    first commit

---
+++ 1ºRelacionEjercicios/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1ºRelacionEjercicios
+{
+    delegate bool ValidarNumero(int x);//Ejercicio 1
+
+    delegate void ValidarNumero2(int x);//Ejercicio 2
+
+    internal class Program
+    {
+
+        static bool MayorAcero(int x) => x > 0;//Ejercicio 1
+
+        //Ejercicio 2
+
+        static void MayorACero2(int x)
+        {
+            Console.WriteLine($"{x} es {(x > 0 ? "mayor" : "menor")} que 0");
+        }
+
+        static void Espar(int x) 
+        {
+            Console.WriteLine($"{x} es {(x % 2 == 0 ? "par" : "impar")}");
+        }
+
+        static void Main(string[] args)
+        {
+            //Ejercicio 1
+            Console.WriteLine("Ejercicio 1 validar número mayor que cero");
+            ValidarNumero validate = MayorAcero;
+            Console.WriteLine("Es '2' mayor que cero: "+ (validate(-2) ? "verdadero" : "falso"));
+
+            //------------------------------------------------------------------------------------
+
+            //Ejercicio 2
+            ValidarNumero2 validate2 = MayorACero2;
+            validate2 += Espar;
+
+            int num = 3;
+            foreach (ValidarNumero2 validarNumero in validate2.GetInvocationList()) 
+            {
+                validarNumero(num);
+            }
+
+        }
+    }
+}
---
+++ 2ºRelacionEjercicios/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2ºRelacionEjercicios
+{
+    //Ejercicio 1
+    delegate double Operacion(int x, int y); //Los he tenido que poner double, porque no se si se puede parsear de alguna forma para que la division que puede delvover decimales se pueda relacionar con un delegate tipo int
+  
+    //Ejercicio 6
+    delegate int Operar(int x, int y);
+    internal class Program
+    {
+        //Ejercicio 1
+        static double Sumar(int x, int y) => x + y;
+
+        //Ejercicio 2
+        static double Restar(int x, int y) => x - y;
+        
+        //Ejercicio 3 
+        static double Multiplicar(int x, int y) => x * y;
+
+        //Ejercicio 4
+        static double Dividir(int x, int y)
+        {
+            if (y == 0)
+                throw new DivideByZeroException("Error: División por cero.");
+
+            return Math.Round( (double)x / y, 2);
+        }
+
+        //Ejercicio 5
+        static void EjecutarOperacion(Operacion op, int x , int y)
+        {
+           Console.WriteLine($"Resultado: {op(x, y)}");
+        }
+
+        //Ejercicio 6
+        static double Suma(int x, int y) => x + y;
+
+        static double Resta(int x, int y) => x - y;
+
+        static double Multiplicacion(int x, int y) => x * y;
+
+
+        static void Main(string[] args)
+        {
+            //Ejercicio 1
+            Console.WriteLine("Ejercicio 1: Delegado básico");
+            Operacion opEj1 = Sumar;
+            Console.WriteLine($"Opercación sumar de 2 + 3: {opEj1(2, 3)}");
+
+            //Ejercicio 2
+            Console.WriteLine("\nEjercicio 2: Multiples métodos");
+            Operacion opEj2 = Sumar;
+            opEj2 += Restar;
+            Console.WriteLine("Ambos metodos con los número 5 y 3, primero suma despues resta");
+            foreach(Operacion metodo in opEj2.GetInvocationList())
+            {
+                Console.WriteLine($"·{metodo(5,3)}");
+            }
+
+            //Ejercicio 3
+            Console.WriteLine("\nEjercicio 3: Uso de Multicast Delegates");
+            Operacion opEj3 = Sumar;
+            opEj3 += Restar;
+            opEj3 += Multiplicar;
+            Console.WriteLine("Todos los métodos con los número 5 y 3, primero suma, segundo resta y tercero multiplicación");
+            foreach (Operacion metodo in opEj3.GetInvocationList())
+            {
+                Console.WriteLine($"·{metodo(5, 3)}");
+            }
+
+            //Ejercicio 4
+            Console.WriteLine("\nEjercicio 4: Delegados con Diferentes Métodos");
+            Operacion opEj4 = Sumar;
+            opEj4 += Restar;
+            opEj4 += Multiplicar;
+            opEj4 += Dividir;
+            Console.WriteLine("Todos los métodos con los número 5 y 3, primero suma, segundo resta y tercero multiplicación");
+            foreach (Operacion metodo in opEj4.GetInvocationList())
+            {
+                try
+                {
+                    Console.WriteLine($"·{metodo(5, 3)}");
+                }
+                catch (DivideByZeroException ex)
+                {
+                    Console.WriteLine(ex.Message); 
+                }
+            }
+
+            //Ejercicio 5
+            Console.WriteLine("\nEjercicio 5: Delegados y Retornos");
+            EjecutarOperacion(Sumar, 5, 3);
+            EjecutarOperacion(Restar, 5, 3);
+            EjecutarOperacion(Multiplicar, 5, 3);
+
+            //Ejercicio 6
+            Console.WriteLine("\nEjercicio 6: Delegados y Nombres");
+            Operacion opEj6 = Suma;
+            opEj6 += Resta;
+            opEj6 += Multiplicacion;
+            Console.WriteLine("Ambos metodos con los número 5 y 3, primero suma despues resta");
+            foreach (Operacion metodo in opEj2.GetInvocationList())
+            {
+                Console.WriteLine($"·{metodo(5, 3)}");
+            }
+
+        }
+    }
+}
